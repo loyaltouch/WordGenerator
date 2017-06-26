b47b6bcdb0f611d540257b26aea305d9b5e205c3
unknown <>
2017-06-27 06:21:42 +0900

    add validate() test code

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,22 @@
-QUnit.test( "hello test", function( assert ) {
+QUnit.test("hello test", assert => {
   assert.ok( 1 == "1", "Passed!" );
 });
+
+QUnit.test("num_maxよりnum_minが大きければ交換", assert => {
+  let w = new WordGenerator();
+  let model = {};
+  model.num_min = 7;
+  model.num_max = 5;
+  result = w.validate(model);
+  assert.equal(result.num_min, 5, "最小値のほうが小さい");
+  assert.equal(result.num_max, 7, "最大値のほうが大きい");
+});
+
+QUnit.test("char_listに文字が入っていなければ既定値を入力", assert => {
+  let w = new WordGenerator();
+  let model = {};
+  model.char_list = "";
+  result = w.validate(model);
+  assert.equal(result.char_list, "ieaou", "規定値ieaou");
+});
+
